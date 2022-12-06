4903af15b6a316b96a500fbeaebccf38ab8974fd
veerendra <20a95a0335@aec.edu.in>
2022-12-06 11:12:44 +0530

    first commit

---
+++ parking_lot.py
@@ -0,0 +1,10 @@
+noOfCars = 0
+noOfBikes = 0
+totalPayment = 0
+
+noOfCars= int(input("no of cars:"))
+noOfBikes= int(input("no of bikes:"))
+totalPayment= (noOfBikes*20) + (noOfCars*40)
+print("total payment would be:",totalPayment)
+
+
---
+++ product_quantity.py
@@ -0,0 +1,10 @@
+product1=0
+product2=0
+product3=0
+
+product1=int(input("product1:"))
+product2=int(input("product2:"))
+product3=int(input("product3:"))
+
+totalQuantity=(product1*40+product2*50+product3*60)
+print("Total Quantity",totalQuantity)
